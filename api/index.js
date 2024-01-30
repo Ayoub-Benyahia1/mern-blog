b23807f1ae0aa3e1db25d17111e275b8f89bad7e
AYOUB BENYAHIA <142797314+Ayoub-Benyahia1@users.noreply.github.com>
2024-01-30 15:01:30 +0100

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
-app.listen(3000, ()=> {
-    console.log('server is runing !!')
-})
+app.listen(3000, () => {
+  console.log("server is runing !!");
+});
